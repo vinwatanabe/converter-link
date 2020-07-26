60e7079cd82deddc243efe819c9ab51805518f60
Vinicius <vinicius_watanabe@hotmail.com>
2020-07-25 23:50:42 -0300

    Updates

---
+++ precache-manifest.0f2623203ef449408d16793a2ed5a8f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c46c90cbfe031c9eac8afe30c253e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2d9837253acd757acb",
+    "url": "/static/css/main.6216afcf.chunk.css"
+  },
+  {
+    "revision": "b786f169ed2776942400",
+    "url": "/static/js/2.1d04d6f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d04d6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2d9837253acd757acb",
+    "url": "/static/js/main.06bdb214.chunk.js"
+  },
+  {
+    "revision": "5ef18625a0c42fa8770e",
+    "url": "/static/js/runtime-main.b34cab63.js"
+  },
+  {
+    "revision": "9895d360932f6aedda4b405d858e1266",
+    "url": "/static/media/btnClose.9895d360.svg"
+  }
+]);
