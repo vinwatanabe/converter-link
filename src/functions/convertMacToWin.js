09067a65f84ca8675ef2798194192d78198ae6d4
Vinicius <vinicius_watanabe@hotmail.com>
2020-10-05 10:45:09 -0300

    Updated the mac server link

--- src/functions/convertMacToWin.js
+++ src/functions/convertMacToWin.js
@@ -1,26 +1,29 @@
-function convertMacToWin(){
-    //Select input field
-    const inputField = document.getElementsByClassName("inputField")[0];
-    const inputText = inputField.value;
+function convertMacToWin() {
+	//Select input field
+	const inputField = document.getElementsByClassName('inputField')[0];
+	const inputText = inputField.value;
 
-    // Replace content and search for /
-    let initialText = inputText.replace("smb://172.16.32.36/ClientesID/", "I:\\");
-    let searchText = initialText.search("/");
+	// Replace content and search for /
+	let initialText = inputText.replace(
+		'smb://10.244.64.170/ClientesID/',
+		'I:\\'
+	);
+	let searchText = initialText.search('/');
 
-    while(searchText > 0) {
-        const finalText = initialText.replace("/", "\\");
-        initialText = finalText;
-        searchText = initialText.search("/");
-    }
+	while (searchText > 0) {
+		const finalText = initialText.replace('/', '\\');
+		initialText = finalText;
+		searchText = initialText.search('/');
+	}
 
-    const convertionResult = initialText;
+	const convertionResult = initialText;
 
-    //insert the convertionResult in the class "result" in LightBox componet
+	//insert the convertionResult in the class "result" in LightBox componet
 
-    const result = document.getElementsByClassName("result")[0];
-    result.innerHTML = convertionResult;
+	const result = document.getElementsByClassName('result')[0];
+	result.innerHTML = convertionResult;
 
-    inputField.value = convertionResult;
+	inputField.value = convertionResult;
 }
 
 export default convertMacToWin;
